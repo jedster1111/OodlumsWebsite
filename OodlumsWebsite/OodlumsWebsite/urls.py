1e3267dea2bf4e4de324ae78b2aa8142b1547da8
Jed <jedster1111@hotmail.co.uk>
2018-01-31 17:07:02 +0000

    Add project files.

---
+++ OodlumsWebsite/OodlumsWebsite/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for OodlumsWebsite.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', OodlumsWebsite.views.home, name='home'),
+    # url(r'^OodlumsWebsite/', include('OodlumsWebsite.OodlumsWebsite.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
