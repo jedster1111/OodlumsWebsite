bdd4f6f513b7d7ff0df177d4399aec9499dacfd7
Jed <jedster1111@hotmail.co.uk>
2018-02-05 18:37:27 +0000

    Made contact form, submits emails to console only at the moment. Also added the functionality to videos page if there are not sellfy links it'll display a coming soon button.

---
+++ OodlumsWebsite/OodlumsWebsite/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Enter name'}))
+    email = forms.EmailField(label='Email', widget=forms.TextInput(attrs={'placeholder': 'Enter email'}))
+    message = forms.CharField(label='Message', widget=forms.Textarea(attrs={'placeholder': 'Enter your message'}))
--- OodlumsWebsite/OodlumsWebsite/views.py
+++ OodlumsWebsite/OodlumsWebsite/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.core.mail import send_mail, EmailMessage, BadHeaderError
+from django.template import Context
+from django.template.loader import get_template
+from .forms import ContactForm
 
 def home(request):
     return render(request, 'oodlums/home.html')
 
 def contact(request):
-    return render(request, 'oodlums/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = request.POST.get('name','')
+            email = request.POST.get('email','')
+            message = request.POST.get('message','')
+            template = get_template('oodlums/contact_template.txt')
+            context = ({
+                    'name' : name,
+                    'email' : email,
+                    'message' : message,
+                })
+            content = template.render(context)
+
+            email = EmailMessage(
+                "Contact form submission",
+                content,
+                "Oodlums" + '',
+                ['jedster1111@hotmail.co.uk'],
+                headers = {'Reply-To' : email}
+                )
+            try:
+                email.send();
+            except BadHeaderError:
+                return HttpResponse('Invalid Header Found')
+            return redirect('contact')
+
+    else:
+        form = ContactForm()
+
+    return render(request, 'oodlums/contact.html', {'form' : form})
