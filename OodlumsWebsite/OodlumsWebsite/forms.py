aa6b532f190083c950491b2aa97a56cc495d4368
Jed <jedster1111@hotmail.co.uk>
2018-02-05 19:07:09 +0000

    Added footer

--- OodlumsWebsite/OodlumsWebsite/forms.py
+++ OodlumsWebsite/OodlumsWebsite/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Enter name'}))
-    email = forms.EmailField(label='Email', widget=forms.TextInput(attrs={'placeholder': 'Enter email'}))
+    name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Enter your name'}))
+    email = forms.EmailField(label='Email', widget=forms.TextInput(attrs={'placeholder': 'Enter your email'}))
     message = forms.CharField(label='Message', widget=forms.Textarea(attrs={'placeholder': 'Enter your message'}))
