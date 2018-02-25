707cd5a11051b1d4984b6069579601bf1e9ac11d
Jed <jedster1111@hotmail.co.uk>
2018-02-25 17:12:32 +0000

    Added secret settings and deployment settings file and moved secret key out of base settings file

--- OodlumsWebsite/OodlumsWebsite/forms.py
+++ OodlumsWebsite/OodlumsWebsite/forms.py
@@ -4,5 +4,3 @@ class ContactForm(forms.Form):
     name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Enter your name'}))
     email = forms.EmailField(label='Email', widget=forms.TextInput(attrs={'placeholder': 'Enter your email'}))
     message = forms.CharField(label='Message', widget=forms.Textarea(attrs={'placeholder': 'Enter your message'}))
-
-    #test
