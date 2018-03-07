51a48bf4db0a98fe409213bea9642c714ea87029
Jed <jedster1111@hotmail.co.uk>
2018-03-07 02:08:11 +0000

    Contact from now sends emails and displays a thanks for email message

--- OodlumsWebsite/OodlumsWebsite/settings.py
+++ OodlumsWebsite/OodlumsWebsite/settings.py
@@ -132,6 +132,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'youremail'
+DEFAULT_FROM_EMAIL = 'youremail'
+EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_PORT = 587
+
 
 from .settings_secret import *
