39d48770affe29c16fef6c426f2094a7b1dfa3e5
Jed <jedster1111@hotmail.co.uk>
2018-03-07 02:08:11 +0000

    Contact from now sends emails and displays a thanks for email message

--- OodlumsWebsite/OodlumsWebsite/views.py
+++ OodlumsWebsite/OodlumsWebsite/views.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail, EmailMessage, BadHeaderError
 from django.template import Context
 from django.template.loader import get_template
 from .forms import ContactForm
+from django.contrib import messages
 
 def home(request):
     return render(request, 'oodlums/home.html')
@@ -23,19 +24,19 @@ def contact(request):
             content = template.render(context)
 
             email = EmailMessage(
-                "Contact form submission",
+                "Oodlums contact form submission",
                 content,
-                "Oodlums" + '',
-                ['jedster1111@hotmail.co.uk'],
-                headers = {'Reply-To' : email}
+                'unnaturalmotionmedia@gmail.com',
+                [email,],
                 )
             try:
                 email.send();
+                messages.info(request, 'Thanks for the message!')
             except BadHeaderError:
                 return HttpResponse('Invalid Header Found')
             return redirect('contact')
 
     else:
-        form = ContactForm()
+         form = ContactForm()
 
     return render(request, 'oodlums/contact.html', {'form' : form})
