00984a3d14b16cf72b4b72e88f86ae972cc6d415
jedster1111 <jedster1111@hotmail.co.uk>
2018-03-12 03:40:27 +0000

    updated settings_deployment for use on pythonanywhere

--- OodlumsWebsite/OodlumsWebsite/settings_deployment.py
+++ OodlumsWebsite/OodlumsWebsite/settings_deployment.py
@@ -1,6 +1,10 @@
 from .settings import *
 
 DEBUG = False
-ALLOWED_HOSTS += [
+ALLOWED_HOSTS = [
     'jedster1111.pythonanywhere.com',
+    'www.oodlums.com',
+    'oodlums.com',
+    'www.oodlums.co.uk',
+    'oodlums.co.uk',
 ]
