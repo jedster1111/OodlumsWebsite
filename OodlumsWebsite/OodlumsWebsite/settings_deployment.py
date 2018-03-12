907fc71bc43192794ebfa92c3e23ec3cd5b76ef2
jedster1111 <jedster1111@hotmail.co.uk>
2018-03-12 16:20:12 +0000

    adjusted email settings

--- OodlumsWebsite/OodlumsWebsite/settings_deployment.py
+++ OodlumsWebsite/OodlumsWebsite/settings_deployment.py
@@ -1,6 +1,18 @@
 from .settings import *
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'oodlums$Data',
+        'USER': 'oodlums',
+        'PASSWORD': 'Myjasmine10',
+        'HOST': 'oodlums.mysql.pythonanywhere-services.com',
+        }
+    }
+
+
 ALLOWED_HOSTS = [
     'jedster1111.pythonanywhere.com',
     'www.oodlums.com',
